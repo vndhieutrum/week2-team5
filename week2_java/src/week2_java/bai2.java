a832cb546f3ae9127f8df153225374bae55e9520
Tien Hien <huynhletienhien@gmail.com>
2024-09-17 19:55:35 +0700

    hiển làm lại

---
+++ week2_java/src/week2_java/bai2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week2_java;
+
+/**
+ *
+ * @author admin
+ */
+public class bai2 {
+    public static void main(String[] args) {
+        String ten = "Huỳnh Lê Tiến Hiển";
+        int tuoi = 19;
+        double chieucao = 1.65;
+        boolean tlt = true;
+        System.out.println("Tên "+ten);
+        System.out.println("Tuổi "+tuoi);
+        System.out.println("Chiều cao "+chieucao+"m");
+        System.out.println("Có thích lập trình không: " +((tlt)?"Có":"Không"));
+    }
+}
