492850e44a5ccc13ebf9766a80d3c03951900bea
hieuvolaptrinh <vndhieuak@gmail.com>
2024-09-17 19:15:16 +0700

    hiếu code thêm

--- week2_java/src/week2_java/bai1.java
+++ week2_java/src/week2_java/bai1.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
 
-/**
- *
- * @author hieuvo
- */
 public class bai1 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("vui lòng nhập tên của bạn");
         String name= sc.nextLine();
         System.out.println("Hello "+name);
+        System.out.println("hiếu viết thêm dòng này");
     }
 }
