37f9873e2fbe9a228383c8cce83c668a645208b5
hieuvolaptrinh <vndhieuak@gmail.com>
2024-09-17 12:00:49 +0700

    hiếu làm xong bài 1 xong hiếu tạo nhánh hiếu, hiếu push lên r nhe

---
+++ week2_java/src/week2_java/bai1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author hieuvo
+ */
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("vui lòng nhập tên của bạn");
+        String name= sc.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
